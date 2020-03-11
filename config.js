b6bddf22303c52375e3c03b7d0217339a8e1f890
Nickolas Lanasa <nlanasa@its.jnj.com>
2020-03-10 22:29:37 -0400

    Personal updates

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 'use strict';
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
+  url: 'nicklanasa.com',
   pathPrefix: '/',
-  title: 'Blog by John Doe',
-  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+  title: 'Nickolas Lanasa brain dump',
+  subtitle: '',
   copyright: '© All rights reserved.',
   disqusShortname: '',
   postsPerPage: 4,
@@ -25,9 +25,9 @@ module.exports = {
     }
   ],
   author: {
-    name: 'John Doe',
-    photo: '/photo.jpg',
-    bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+    name: 'Nickolas Lanasa',
+    photo: '/photo.jpeg',
+    bio: 'Father. Son. Brother. Uncle. Engineer @ Johnson & Johnson',
     contacts: {
       email: '',
       facebook: '#',
